2a97c2a129c36532d5f697a835ee34a1f9f29a9e
Kaustubh <saikaustubh10@gmail.com>
2021-03-07 08:39:11 +0530

    completed navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
-// import Dropdown from './components/Dropdown';
+import React, { Component, useState } from 'react';
+import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
-// import Accordian from './components/Accordion';
-// import Search from './components/Search';
+import Accordian from './components/Accordion';
+import Search from './components/Search';
+import Route from './components/Route';
+import Header from './components/Header';
 
-// const items = [
-//     {
-//         title: 'what is react?',
-//         content: 'React is a fron end javascript frameword'
-//     },
-//     {
-//         title: 'why use react?',
-//         content: 'React is the best javascript library'
-//     },
-//     {
-//         title: 'How do you use React?',
-//         content: 'You use React by creating components'
-//     }
-// ];
+const items = [
+    {
+        title: 'what is react?',
+        content: 'React is a fron end javascript frameword'
+    },
+    {
+        title: 'why use react?',
+        content: 'React is the best javascript library'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+];
 
 const options = [
     {
@@ -35,14 +37,19 @@ const options = [
 ];
 
 const App = () => {
-    
 
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
+            <Header/>
+            <Route path="/"><Accordian items={items}/></Route>
+            <Route path="/list"><Search /></Route>
+            <Route path="/dropdown"><Dropdown label="select a color" options={options} selected={selected} onSelectedChange={setSelected}/></Route>
+            <Route path="/translate"><Translate /></Route>
             {/* <Accordian items={items}/> */}
             {/* <Search /> */}
             {/* <Dropdown selected={selected} onSelectedChange={setSelected} options={options}/> */}
-            <Translate />
+            {/* <Translate /> */}
         </div>
     );
 };
